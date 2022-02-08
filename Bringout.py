e2480eac6b898538ee0244bc19998e7b3904a7bf
Joe Martin <joemartinky@gmail.com>
2022-02-07 19:17:35 -0500

    first commit

---
+++ Bringout.py
@@ -0,0 +1,9 @@
+def bringoutdead():
+    bringdate = input('What day is it?')
+    if bringdate == 'Monday' or bringdate == 'Thursday':
+        print('Bring out your dead!')
+    else:
+        print('Do not bring out the dead.')
+
+
+bringoutdead()
---
+++ Witchtest.py
@@ -0,0 +1,11 @@
+def witchtest():
+    witch = input('Enter the weight of the suspect witch.')
+    if float(witch) >= 125 and float(witch) <= 135:
+        print('She weighs the same as a duck.  So she is a witch!')
+    else:
+        print('She is not a witch, even if she turned you into a Newt and you got over it')
+
+
+witchtest()
+
+
