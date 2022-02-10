e6091c1618b5aaafc7c4583cd2daac714bda0f90
Joe Martin <joemartinky@gmail.com>
2022-02-10 17:30:41 -0500

    Updated files to look and perform better.

--- Bringout.py
+++ Bringout.py
@@ -1,9 +1,15 @@
 def bringoutdead():
-    bringdate = input('What day is it?')
-    if bringdate == 'Monday' or bringdate == 'Thursday':
-        print('Bring out your dead!')
+    bringdate = input("What day is it?\n")
+    if bringdate == "Monday" or bringdate == "Thursday":
+        print("Bring out your dead!\n")
     else:
-        print('Do not bring out the dead.')
+        print("Don't bring out the dead.\n")
+
+    repeat = input("Would you like to re-check?(y/n)?\n")
+    if repeat == "y" or repeat == "Y":
+        bringoutdead()
+    else:
+        exit()
 
 
 bringoutdead()
--- Witchtest.py
+++ Witchtest.py
@@ -1,10 +1,15 @@
 def witchtest():
-    witch = input('Enter the weight of the suspect witch.')
+    witch = input("Enter the weight of the suspect witch.\n")
     if float(witch) >= 125 and float(witch) <= 135:
-        print('She weighs the same as a duck.  So she is a witch!')
+        print("She weighs the same as a duck.  So she's a witch!\n")
     else:
-        print('She is not a witch, even if she turned you into a Newt and you got over it')
+        print("She's not a witch, even if she turned you into a Newt and you got over it\n")
 
+    repeat = input("Would you like to retest?(y/n)\n")
+    if repeat == "y" or repeat == "Y":
+        witchtest()
+    else:
+        exit()
 
 witchtest()
 
