8b8367751e0ad9f06a207d6650acc694609a596f
Joe Martin <joemartinky@gmail.com>
2022-02-24 19:21:50 -0500

    updated files and programs

--- Pitching_Analysis.py
+++ Pitching_Analysis.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+# from matplotlib import pyplot as plt
 
 
 # Pitching.csv came from the Kaggle database located at
@@ -11,25 +11,52 @@ import matplotlib as plt
 # 1. Use pd.read_csv('/Pitching.csv') to create a DataFrame called pitching.
 # 2. Use .head() to display the first 5 rows of the dataset.
 
-pitching = pd.read_csv('Pitching.csv')
+pitching = pd.read_csv('data\Pitching.csv')
 pitching.head()
 
 # Since the data contains entries from 1871 to 2015, need to narrow the size of the
 # database.  In 1950, Major League Baseball set the pitching mound at 15 inches high.
 # Because of complaints from batters in "The year of the pitcher (1968)", MLB lowered
-# the mound height to ten inches. So, data will cover from 1950 to 2015.
+# the mound height to ten inches. So, data will cover from 1965 to 1972, four years before
+# and after the mound height was lowered.
 
-# Update the dataset to include only rows from 1950 to 2015, had a minumu of 20 starts
+# Update the dataset to include only rows from 1965 to 1972, had a minumu of 20 starts
 # each season, and have no relief appearances,  In addition, keep only the first 20 columns.
 
-pitching = pitching[(pitching['yearID'] >= 1950) & (pitching['GS'] >= 20) & (pitching['G'] == pitching['GS'])].iloc[:, :20]
+pitching = pitching[(pitching['yearID'] >= 1965) & (pitching['yearID'] <= 1972) & (pitching['GS'] >= 20) & (pitching['G'] == pitching['GS'])].iloc[:, :20]
 pitching = pitching.drop(['stint', 'W', 'L', 'CG', 'SHO', 'SV'], axis=1)
 pitching.head()
 
-# The pitching sample script on Kaggle has calculation for other pitching metrics.  All I want to show is the averages of
-# the Earned Run Average (ERA) from 1950 to 2015 to see if lowering the mount height in 1969 did have an impact on pitching ERA.
+# The pitching sample script on Kaggle has calculation for other pitching metrics.  I decided to include some of those metrics for 
+# possible future use.
+# the analysis here is to analyze the Earned Run Average (ERA) from 1965 to 1972 to see if lowering the mount height in 1969
+#  did have an impact on pitching ERA.
 
-# Create a line chart showing the averages per year
+# The first set up is to create a column that computes the number of innings pitched.  The data has a field called IPouts which
+# shows the total outs pitched by pitcher.  So, simply, take this number and divide by 3 to get the innings.  Round the decimal to two digits.
+# Once the column is created, there is no need for the IPouts column, so that will be deleted.
 
-plt =  pitching.groupby('yearID').ERA.mean().plot.line()
+pitching['IP'] = ((pitching['IPouts'])/3).round(2)
+del pitching['IPouts']
+pitching.head()
+
+# With the number of innings pitched, we can now calculate the strikeouts per nine innings and the walks per nine innings.
+# These two columns will be showing to see if lowering impacted the strikeouts per nine and walks per nine innings.
+
+# For the strikeouts per nine, perform the calculation of selecting the `SO` column, multiplying it by 9, and dividing by the `IP` column.
+# Use .round(2) to round the results to 2 decimal places.
+
+pitching['SO9'] = (pitching['SO'] * 9 / pitching['IP']).round(2)
+
+# For the walks per nine inning, perform the calculation of selecting the `BB` column,  multiplying it by 9, and then dividing by the `IP` column.
+# Use .round(2) to round the results to 2 decimal places.
+# Use .head() to display the first 5 rows of the updated dataset.
+
+pitching['BB9'] = (pitching['BB'] * 9 / pitching['IP']).round(2)
+pitching.head()
+
+# Eventually, I plan to show the information in a graph.  For this class, I plan to just show the dataframe.
+# Grouping the yearID, show the average ERA, SO9 and BB9.
+
+pitching.groupby('yearID').ERA.mean().round(2)
 
